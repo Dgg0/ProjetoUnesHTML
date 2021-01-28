ce19250a517b2a8191a8838780b017e4e426839e
DiogoH <69657046+Dgg0@users.noreply.github.com>
2021-01-28 19:52:08 -0300

    Add files via upload

---
+++ ProjetoUnes/contato.php
@@ -0,0 +1,9 @@
+<?php
+	$Email = $_POST['email'];
+	$Assunto = $_POST['assunto'];
+	$Descricao = $_POST['descricao'];
+
+	echo 'E-mail: '.$Email.'<br>';
+	echo 'Assunto: '.$Assunto.'<br>';
+	echo 'Descricao: '.$Descricao;
+?>
